84395864bb985f7207557a5e3a6077f597005201
William <william.jose.wjd@gmail.com>
2022-07-03 10:31:55 -0300

    feat: criando log na camada de serviço de usuario

--- src/main/java/com/antares/services/impl/UsuarioServiceImpl.java
+++ src/main/java/com/antares/services/impl/UsuarioServiceImpl.java
@@ -3,6 +3,8 @@ package com.antares.services.impl;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,7 +23,8 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UsuarioServiceImpl implements UsuarioService {
-
+	private static final Logger logger = LoggerFactory.getLogger(UsuarioServiceImpl.class);
+	
 	private final UsuarioRepository usuarioRepository;
 	private final ModelMapper modelMapper;
 
@@ -30,11 +33,16 @@ public class UsuarioServiceImpl implements UsuarioService {
 	@Override
 	public UsuarioDTO save(UsuarioCadastroDTO usuarioCadastroDTO) {
 		try {
+			logger.info("** Cadastro de Usuario - Iniciando o cadastro de usuário");
+			
 			String encoderPassword = passwordEncoder.encode(usuarioCadastroDTO.getPassword());
 			usuarioCadastroDTO.setPassword(encoderPassword);
 			Usuario usuario = usuarioRepository.save(modelMapper.map(usuarioCadastroDTO, Usuario.class));
+			
+			logger.info("** Cadastro de Usuario - Finalizando o cadastro de usuário");
 			return modelMapper.map(usuario, UsuarioDTO.class);
 		}catch (Exception e) {
+			logger.info("** Cadastro de Usuario - Ocorreu um erro ao salvar o usuário {}", usuarioCadastroDTO);
 			throw new DataIntegrityViolationException("Ocorreu um erro ao salvar usuário.", e.getCause());
 		}
 	}
@@ -42,8 +50,10 @@ public class UsuarioServiceImpl implements UsuarioService {
 	@Override
 	public UsuarioDTO update(Integer id, UsuarioUpdateDTO usuarioUpdateDTO) {
 		try {
+			logger.info("** Atualizacao de Usuario - Iniciando a atualizacao de usuário");
 			Optional<Usuario> user = findUserById(id);
-
+			
+			logger.info("** Atualizacao de Usuario - Valida se o usuario já existe e altera seus atributos");
 			if (user.isPresent()) {
 				user.get().setNome(usuarioUpdateDTO.getNome());
 				user.get().setDataNascimento(usuarioUpdateDTO.getDataNascimento());
@@ -55,11 +65,14 @@ public class UsuarioServiceImpl implements UsuarioService {
 				user.get().setGenero(usuarioUpdateDTO.getGenero());
 				user.get().setEmail(usuarioUpdateDTO.getEmail());
 			}
-
+			
+			logger.info("** Atualizacao de Usuario - Persiste as atualizacoes na base de dados");
 			Usuario usuario = usuarioRepository.save(user.get());
-
+			
+			logger.info("** Atualizacao de Usuario - Finaliza as alteracoes de usuario");
 			return modelMapper.map(usuario, UsuarioDTO.class);
 		} catch (Exception e) {
+			logger.info("** Atualizacao de Usuario - Ocorreu um erro ao alterar os dados do usuario de id: {}", id);
 			throw new DataIntegrityViolationException("Ocorreu um erro ao atualizar esse usuário.", e.getCause());
 		}
 	}

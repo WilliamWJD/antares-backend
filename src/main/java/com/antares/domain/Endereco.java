646afb9541aea2c3a32e0450aa8b7126bd5cae53
William <william.jose.wjd@gmail.com>
2022-07-03 09:49:42 -0300

    feat: criando dominio endereco

---
+++ src/main/java/com/antares/domain/Endereco.java
@@ -0,0 +1,34 @@
+package com.antares.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	private String estado;
+	private String cidade;
+	private String bairro;
+	private String cep;
+	private String logradouro;
+	private Integer numero;
+}

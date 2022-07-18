ea3d24d994491ce61f582122a1d4a307b987dca3
William <william.jose.wjd@gmail.com>
2022-07-18 19:18:58 -0300

    refactor: removendo importacoes nao utilizadas

--- src/main/java/com/antares/repository/EnderecoImovelRepository.java
+++ src/main/java/com/antares/repository/EnderecoImovelRepository.java
@@ -1,7 +1,5 @@
 package com.antares.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,17 +7,6 @@ import org.springframework.data.repository.query.Param;
 import com.antares.domain.EnderecoImovel;
 
 public interface EnderecoImovelRepository extends JpaRepository<EnderecoImovel, Integer>{
-//	@Query(value = "select \r\n"
-//			+ "* \r\n"
-//			+ "from endereco_imovel ei \r\n"
-//			+ "inner join imovel i ON i.endereco_id = ei.id \r\n"
-//			+ "inner join usuario u on u.id = i.usuario_id \r\n"
-//			+ "where ei.cep = :cep \r\n"
-//			+ "and ei.numero = :numero \r\n"
-//			+ "and i.id <> :id\r\n"
-//			+ "and u.id = :userId", nativeQuery = true)
-//	Optional<EnderecoImovel> findByCepAndNumero(@Param("cep") String cep,  @Param("numero")Integer numero,@Param("id")Integer id, @Param("userId") Integer userId);
-
 	@Query(value = "select * from endereco_imovel ei \r\n"
 			+ "inner join imovel i on ei.id = i.endereco_id\r\n"
 			+ "inner join usuario u on i.usuario_id = u.id\r\n"
